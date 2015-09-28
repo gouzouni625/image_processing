4114af3886c5ab66f636e9a9c264de41220c6c26
Georgios Ouzounis <georgios.ouzounis@geogebra.at>
2015-09-28 23:58:46 +0300

    Added Drawer class.

---
+++ src/main/java/drawing/Drawer.java
@@ -0,0 +1,12 @@
+package main.java.drawing;
+
+import main.java.image.Image;
+
+public class Drawer{
+  public static Image drawLine(Image image, double xStart, double yStart, double xEnd, double yEnd, int thickness){
+
+
+    return image;
+  }
+
+}
