1f4c3b43f8f77e2ed14957682c97a864f6a4a866
Georgios Ouzounis <georgios.ouzounis@geogebra.at>
2015-10-12 23:02:15 +0300

    Changed resize method to nearest neighbour.

--- src/main/java/org/improc/core/Core.java
+++ src/main/java/org/improc/core/Core.java
@@ -3,54 +3,31 @@ package org.improc.core;
 import org.improc.image.Image;
 
 public class Core {
-  public static Image resize(Image image, int newWidth, int newHeight){
-    Image resizedImage = new Image(newWidth, newHeight);
 
+  public static Image resize(Image image, int newWidth, int newHeight) {
     int oldWidth = image.getWidth();
     int oldHeight = image.getHeight();
 
-    double relativeWidth = (double)(oldWidth) / ((double)(newWidth));
-    double relativeHeight = (double)(oldHeight) / ((double)(newHeight));
+    Image resizedImage = new Image(newWidth, newHeight);
 
-    double factorX = 0;
+    int relativeWidth = (oldWidth << 16) / newWidth;
+    int relativeHeight = (oldHeight << 16) / newHeight;
+
     int oldX = 0;
-    double deltaX = 0;
-    for(int x = 0;x < newWidth;x++){
-      double factorY = 0;
+    int factorX = relativeWidth;
+    for(int x = 0;x < newWidth;x++) {
+
       int oldY = 0;
-      double deltaY = 0;
+      int factorY = relativeHeight;
+      for(int y = 0;y < newHeight;y++) {
+        resizedImage.setPixel(x, y, image.getPixel(oldX, oldY));
 
-      for(int y = 0;y < newHeight;y++){
-
-        double newPixelValue;
-        if((oldX == oldWidth - 1) && oldY == (oldHeight - 1)){
-          newPixelValue = image.getPixel(oldX, oldY) * (1 - deltaX) * (1 - deltaY);
-        }
-        else if(oldX == oldWidth - 1){
-          newPixelValue = image.getPixel(oldX, oldY) * (1 - deltaX) * (1 - deltaY) +
-              image.getPixel(oldX, oldY + 1) * (1 - deltaX) * deltaY;
-        }
-        else if(oldY == oldHeight - 1){
-          newPixelValue = image.getPixel(oldX, oldY) * (1 - deltaX) * (1 - deltaY) +
-              image.getPixel(oldX + 1, oldY) * deltaX * (1 - deltaY);
-        }
-        else{
-          newPixelValue = image.getPixel(oldX, oldY) * (1 - deltaX) * (1 - deltaY) +
-              image.getPixel(oldX + 1, oldY) * deltaX * (1 - deltaY) +
-              image.getPixel(oldX, oldY + 1) * (1 - deltaX) * deltaY +
-              image.getPixel(oldX + 1, oldY + 1) * deltaX * deltaY;
-        }
-
-        resizedImage.setPixel(x, y, (int)(newPixelValue));
-
+        oldY = factorY >> 16;
         factorY += relativeHeight;
-        oldY = (int)(factorY);
-        deltaY = factorY - oldY;
       }
 
+      oldX = factorX >> 16;
       factorX += relativeWidth;
-      oldX = (int)(factorX);
-      deltaX = factorX - oldX;
     }
 
     return resizedImage;
